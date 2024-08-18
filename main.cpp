09c7fb9d1b329cdc47eec8c54d37cfe2a936e88b
jong-k <fully.charged07@gmail.com>
2024-08-18 21:33:18 +0900

    feat: 문자열 탐색 find 함수 추가

--- main.cpp
+++ main.cpp
@@ -1,22 +1,28 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main() {
-  const int a = 13;
-  const int b = 4;
-  // 정수형 비교 연산
-  cout << (a == b) << endl; // 0(숫자)
-  cout << (a != b) << endl; // 1
-  cout << (a > b) << endl; // 1
-  cout << (a < b) << endl; // 0
-  cout << (a >= b) << endl; // 1
-  cout << (a <= b) << endl; // 0
+  string str = "Hello, C++ World!";
 
-  // 정수형 비트 연산
-  // AND 연산
-  cout << (a & b) << endl; // 4
-  // OR 연산
-  cout << (a | b) << endl; // 13
+  size_t pos0 = str.find('l');
+  size_t pos1 = str.find("Hello");
+  size_t pos2 = str.find("o, ");
+
+  cout << pos0 << endl; // 2
+  cout << pos1 << endl; // 0
+  cout << pos2 << endl; // 4
+
+  // 존재하지 않는 문자열 찾기 1
+  size_t start_index = 15;
+  size_t pos3 = str.find("World", start_index);
+  // string::npos
+  cout << pos3 << endl; // 18446744073709551615
+
+  // 존재하지 않는 문자열 찾기 2
+  size_t pos4 = str.find("Python");
+  cout << pos4 << endl; // 18446744073709551615
+
   return 0;
 }
